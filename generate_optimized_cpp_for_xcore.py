9a5125de37c59f72b40601cb12ea9f34bc862ab9
panickal-xmos <deepakpanickal@xmos.com>
2024-05-06 14:42:12 +0100

    Update

--- generate_optimized_cpp_for_xcore.py
+++ generate_optimized_cpp_for_xcore.py
@@ -1,26 +1,38 @@
 from xmos_ai_tools import xformer
 
-model_path = ""
-optimized_model_path = "host_app/src/model.tflite"
+TFLITE_MODEL_PATH = "models/MODEL.tflite"
+OPTIMIZED_MODEL_PATH = "host_app/src/model.tflite"
+
+OPT_MODEL_PATH = "src/model.tflite"
 
 print("Generating host app cpp files for model...")
 xformer.convert(
-    model_path,
-    optimized_model_path,
+    TFLITE_MODEL_PATH,
+    OPTIMIZED_MODEL_PATH,
     {
         "xcore-thread-count": "5",
+        # "xcore-conv-err-threshold": "3",
     },
 )
 
-optimized_model_path = "device_app/src/model.tflite"
-
+OPTIMIZED_MODEL_PATH = "device_app/src/model.tflite"
+# WEIGHT_PARAMS_PATH = "src/model_weights.params"
+# FLASH_IMAGE_PATH = "src/xcore_flash_binary.out"
 print("Generating device app cpp files for model...")
 xformer.convert(
-    model_path,
-    optimized_model_path,
+    TFLITE_MODEL_PATH,
+    OPTIMIZED_MODEL_PATH,
     {
         "xcore-thread-count": "5",
+        # "xcore-conv-err-threshold": "3",
     },
 )
+
+# Generate flash binary
+# xformer.generate_flash(
+#     output_file=FLASH_IMAGE_PATH,
+#     model_files=[OPTIMIZED_MODEL_PATH],
+#     param_files=[WEIGHT_PARAMS_PATH],
+# )
 
 print("Done!")
